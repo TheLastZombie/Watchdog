b3367460f9507299610e28782d29e33ed785dccd
TLZ <roesch.eric@protonmail.com>
2020-10-21 14:51:12 +0200

    Send screenshots as one file

--- commands/tools/screenshot.js
+++ commands/tools/screenshot.js
@@ -26,24 +26,41 @@ module.exports = class ScreenshotCommand extends commando.Command {
     const screenshot = require('screenshot-desktop')
     const sharp = require('sharp')
 
-    screenshot.all().then(imgs => {
-      imgs.forEach(img => {
-        sharp(img)
-          .blur(config.blur.screenshot || 0.3)
+    screenshot.listDisplays().then(displays => {
+      screenshot.all().then(imgs => {
+        imgs = imgs.map((x, i) => {
+          return {
+            input: x,
+            top: displays[i].offsetY,
+            left: displays[i].offsetX
+          }
+        })
+
+        sharp({
+          create: {
+            width: Math.max(...displays.map(x => x.width + x.offsetX)),
+            height: Math.max(...displays.map(x => x.height + x.offsetY)),
+            channels: 4,
+            background: { r: 0, g: 0, b: 0, alpha: 0 }
+          }
+        })
+          .composite(imgs)
+          .jpeg()
           .toBuffer()
           .then(data => {
-            msg.channel.send({
-              files: [data]
-            }).then(msg.react(config.reactions.success))
+            sharp(data)
+              .blur(config.blur.screenshot || 0.3)
+              .toBuffer()
+              .then(data => {
+                msg.channel.send({
+                  files: [data]
+                }).then(msg.react(config.reactions.success))
+              })
           })
-          .catch(err => {
-            msg.react(config.reactions.error)
-            return msg.channel.send(err.toString())
-          })
+      }).catch(err => {
+        msg.react(config.reactions.error)
+        return msg.channel.send(err.toString())
       })
-    }).catch(err => {
-      msg.react(config.reactions.error)
-      return msg.channel.send(err.toString())
     })
   }
 }
