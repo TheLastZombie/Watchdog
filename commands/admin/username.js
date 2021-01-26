796fb4db568f1f0aa6fbb1ce9021b155f7c3c011
TLZ <roesch.eric@protonmail.com>
2021-01-26 20:40:22 +0100

    Add username command

---
+++ commands/admin/username.js
@@ -0,0 +1,35 @@
+const commando = require('discord.js-commando')
+
+module.exports = class UsernameCommand extends commando.Command {
+  constructor (client) {
+    super(client, {
+      name: 'username',
+      aliases: ['name'],
+      group: 'admin',
+      memberName: 'username',
+      description: 'Sets the bot\'s Discord username.',
+      examples: [
+        'username Watchdog'
+      ],
+      ownerOnly: true,
+      clientPermissions: [
+        'ADD_REACTIONS'
+      ],
+      args: [
+        {
+          key: 'name',
+          label: 'Name',
+          prompt: 'Please enter the wanted username. You can also specify it directly after the username command.',
+          type: 'string'
+        }
+      ]
+    })
+  }
+
+  async run (msg, args) {
+    const config = require('../../config')
+    msg.react(config.reactions.progress)
+
+    this.client.user.setUsername(args.name).then(msg.react(config.reactions.success))
+  }
+}
